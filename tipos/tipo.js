239588317e5878002155f06c1b786158ced518d9
Emílio Silva <emiliosarges@gmail.com>
2025-06-27 01:11:55 -0300

    tipos
    Entendendo os tipos em TypeScript

---
+++ tipos/tipo.js
@@ -0,0 +1,114 @@
+"use strict";
+// string
+let nome = 'João';
+console.log(nome);
+//nome = 28 --> aponta erro.
+// numbers
+let idade = 27;
+// idade = 'Luiza' --> aponta erro
+idade = 27.5;
+console.log(idade);
+//boolean
+let possuiHobbies = false;
+//possuiHobbies = 1 --> aponta erro
+console.log(possuiHobbies);
+//tipos explicitos
+let minhaIdade;
+minhaIdade = 27;
+console.log(typeof minhaIdade);
+// minhaIdade = 'idade é 27' --> aponta erro
+//Se a variável não for inicializada ou ter seu tipo definido será tratada como any (aceita ser alterada sem apontar erro)
+let soma;
+soma = 2;
+let logico;
+logico = true;
+let texto;
+texto = 'texto';
+let qualquer;
+qualquer = 'texto';
+qualquer = 1;
+let qualquer2;
+qualquer2 = 2;
+qualquer2 = 'texto2';
+console.log(typeof soma);
+console.log(typeof logico);
+console.log(typeof texto);
+console.log(typeof qualquer);
+console.log(typeof qualquer2);
+// array
+let hobbies = ['Cozinhar', 'Praticar Esportes'];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+hobbies = [100, 5, 633]; //não aponta erro, pois o array foi declarado do tipo any
+console.log(hobbies);
+let numeros = [50, 23]; //criando um array de números e atribuindo valor
+console.log(numeros);
+// tuplas
+// Em TypeScript, uma tupla é um tipo de array com comprimento fixo e tipos definidos para cada posição.
+let pessoa;
+pessoa = ["João", 30]; // ok
+//pessoa = [30, "João"]; // erro: ordem e tipos errados
+console.log(pessoa);
+let endereco = ['Avenida Principal', 444];
+console.log(endereco);
+// enums
+//Em TypeScript, um enum (abreviação de enumeration) é uma forma de definir um conjunto nomeado de valores constantes, facilitando a leitura e manutenção do código.
+var Cor;
+(function (Cor) {
+    Cor[Cor["Cinza"] = 0] = "Cinza";
+    Cor[Cor["Verde"] = 100] = "Verde";
+    Cor[Cor["Azul"] = 10] = "Azul";
+    Cor[Cor["Laranja"] = 11] = "Laranja";
+    Cor[Cor["Amarelo"] = 12] = "Amarelo";
+    Cor[Cor["Vermelho"] = 100] = "Vermelho"; //Não gera problema nem erro duplicar o valor
+})(Cor || (Cor = {}));
+//A atribuição de valores é apenas para demonstração, porém o mais comum é deixar os valores sendo atribuídos de forma automática. 
+var Status;
+(function (Status) {
+    Status[Status["Ativo"] = 0] = "Ativo";
+    Status[Status["Inativo"] = 1] = "Inativo";
+    Status[Status["Pendente"] = 2] = "Pendente";
+})(Status || (Status = {}));
+let s = Status.Ativo; // s = 0
+// Características:
+// Pode ser numérico (começa do 0 por padrão) ou string
+// Ajuda a evitar valores mágicos no código (ex: "ativo", "inativo")
+// Permite melhor autocompletar e validação de tipo
+let minhaCor = Cor.Cinza;
+console.log(minhaCor);
+console.log(Cor.Azul);
+console.log(Cor.Verde);
+console.log(Cor.Laranja, Cor.Amarelo);
+console.log(Cor.Verde, Cor.Vermelho);
+//Exemplo com string enum:
+(function (Cor) {
+    Cor["Preto"] = "preto";
+    Cor["Branco"] = "branco";
+})(Cor || (Cor = {}));
+// Exemplo com enum e switch:
+var StatusPedido;
+(function (StatusPedido) {
+    StatusPedido[StatusPedido["Pendente"] = 0] = "Pendente";
+    StatusPedido[StatusPedido["Processando"] = 1] = "Processando";
+    StatusPedido[StatusPedido["Enviado"] = 2] = "Enviado";
+    StatusPedido[StatusPedido["Entregue"] = 3] = "Entregue";
+})(StatusPedido || (StatusPedido = {}));
+function verificarStatus(status) {
+    switch (status) {
+        case StatusPedido.Pendente:
+            console.log("O pedido está pendente.");
+            break;
+        case StatusPedido.Processando:
+            console.log("O pedido está sendo processado.");
+            break;
+        case StatusPedido.Enviado:
+            console.log("O pedido foi enviado.");
+            break;
+        case StatusPedido.Entregue:
+            console.log("O pedido foi entregue.");
+            break;
+        default:
+            console.log("Status desconhecido.");
+    }
+}
+verificarStatus(StatusPedido.Enviado); // Saída: O pedido foi enviado.
---
+++ tipos/tipo.ts
@@ -0,0 +1,131 @@
+// string
+let nome = 'João'
+console.log(nome)
+//nome = 28 --> aponta erro.
+
+// numbers
+let idade = 27
+// idade = 'Luiza' --> aponta erro
+idade = 27.5
+console.log(idade)
+
+//boolean
+let possuiHobbies = false
+//possuiHobbies = 1 --> aponta erro
+console.log(possuiHobbies)
+
+//tipos explicitos
+let minhaIdade: number
+minhaIdade = 27
+console.log(typeof minhaIdade)
+// minhaIdade = 'idade é 27' --> aponta erro
+
+
+//Se a variável não for inicializada ou ter seu tipo definido será tratada como any (aceita ser alterada sem apontar erro)
+let soma: number
+soma = 2
+let logico: boolean
+logico = true
+let texto: string
+texto = 'texto'
+let qualquer: any
+qualquer = 'texto'
+qualquer = 1
+let qualquer2
+qualquer2 = 2
+qualquer2 = 'texto2'
+console.log(typeof soma);
+console.log(typeof logico);
+console.log(typeof texto);
+console.log(typeof qualquer);
+console.log(typeof qualquer2);
+
+// array
+let hobbies: any[] = ['Cozinhar', 'Praticar Esportes']
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+
+hobbies = [100, 5, 633] //não aponta erro, pois o array foi declarado do tipo any
+console.log(hobbies);
+
+let numeros: number[] = [50, 23] //criando um array de números e atribuindo valor
+console.log(numeros);
+
+// tuplas
+// Em TypeScript, uma tupla é um tipo de array com comprimento fixo e tipos definidos para cada posição.
+let pessoa: [string, number];
+pessoa = ["João", 30]; // ok
+//pessoa = [30, "João"]; // erro: ordem e tipos errados
+console.log(pessoa);
+
+
+let endereco: [string, number] = ['Avenida Principal', 444]
+console.log(endereco)
+
+// enums
+//Em TypeScript, um enum (abreviação de enumeration) é uma forma de definir um conjunto nomeado de valores constantes, facilitando a leitura e manutenção do código.
+enum Cor {
+    Cinza, //0
+    Verde = 100, //100
+    Azul = 10,  //10
+    Laranja, // 11
+    Amarelo, // 12
+    Vermelho = 100 //Não gera problema nem erro duplicar o valor
+}
+
+//A atribuição de valores é apenas para demonstração, porém o mais comum é deixar os valores sendo atribuídos de forma automática. 
+
+enum Status {
+    Ativo,
+    Inativo,
+    Pendente
+}
+let s: Status = Status.Ativo; // s = 0
+
+// Características:
+// Pode ser numérico (começa do 0 por padrão) ou string
+// Ajuda a evitar valores mágicos no código (ex: "ativo", "inativo")
+// Permite melhor autocompletar e validação de tipo
+
+let minhaCor: Cor = Cor.Cinza
+console.log(minhaCor);
+console.log(Cor.Azul);
+console.log(Cor.Verde);
+console.log(Cor.Laranja, Cor.Amarelo);
+console.log(Cor.Verde, Cor.Vermelho);
+
+//Exemplo com string enum:
+enum Cor {
+    Preto = "preto",
+    Branco = "branco",
+}
+
+// Exemplo com enum e switch:
+
+enum StatusPedido {
+  Pendente,
+  Processando,
+  Enviado,
+  Entregue
+}
+
+function verificarStatus(status: StatusPedido) {
+  switch (status) {
+    case StatusPedido.Pendente:
+      console.log("O pedido está pendente.");
+      break;
+    case StatusPedido.Processando:
+      console.log("O pedido está sendo processado.");
+      break;
+    case StatusPedido.Enviado:
+      console.log("O pedido foi enviado.");
+      break;
+    case StatusPedido.Entregue:
+      console.log("O pedido foi entregue.");
+      break;
+    default:
+      console.log("Status desconhecido.");
+  }
+}
+
+verificarStatus(StatusPedido.Enviado); // Saída: O pedido foi enviado.
